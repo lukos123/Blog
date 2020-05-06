115725e42d2a8db757215bf5e3a2e3255938319b
lukos123 <63793389+lukos123@users.noreply.github.com>
2020-05-06 13:42:45 +0300

    bvnbv

---
+++ js/script.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function(){
+	jQuery('.sub .p1').on('click' , function(){
+		jQuery('.header__submenu').toggleClass('active');
+		
+	});
+	jQuery('.header__LearningApps p').on('click' , function(){
+		jQuery('.LearningApps').toggleClass('active');
+		
+	});
+	jQuery('.header__GoogleForms p').on('click' , function(){
+		jQuery('.GoogleForms').toggleClass('active');
+		
+	});
+	jQuery('.header__Project p').on('click' , function(){
+		jQuery('.Project').toggleClass('active');
+		
+	});
+	jQuery('.header__GoogleMaps p').on('click' , function(){
+		jQuery('.GoogleMaps').toggleClass('active');
+		
+	});
+
+
+});
